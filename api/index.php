13f134fea8c41cdb6f9437b88e582ea32cc6f89f
Paulo <paulovelho@paulovelho.com>
2022-08-20 16:21:05 -0300

    adding authorization header

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-	header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+	header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Authorization');
 	header('Access-Control-Allow-Origin: *');
 	header('Content-Type: application/json, charset=utf-8');
 
